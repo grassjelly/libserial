a1429cebf931b4b87e6215c93e3458e5795b6c65
Juan Miguel Jimeno <jimenojmm@gmail.com>
2017-08-25 18:41:58 +0800

    added boost installation

--- installme.sh
+++ installme.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-
+sudo apt-get install libboost-all-dev
 sudo apt-get install dh-autoreconf
 make -f Makefile.dist
 ./configure 
 make
 make install
+echo "export LD_LIBRARY_PATH=/usr/local/lib" >> ~/.bashrc
